616c3972363c1589f630d1f2b188c3855299f15a
wsquarepa <wsquarepa@gmail.com>
2021-05-30 18:24:12 -0700

    Update blooket script

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 const reqip = require("request-ip")
-const crypto = require('crypto');
 
 const port = 8420
 
@@ -18,22 +17,9 @@ http.createServer((req, res) => {
         content = fs.readFileSync("./index.html")
     } else if (urlObj.pathname.endsWith(".js")) {
         if (!urlObj.pathname.includes("index")) {
-            if (req.method == "GET") {
-                if (fs.existsSync("." + urlObj.pathname)) {
-                    if ((req.headers.code && req.headers.code == "informativeDonkey") || (urlObj.query.code && urlObj.query.code == "informativeDonkey")) {
-                        responseCode = 200
-                        content = fs.readFileSync("." + urlObj.pathname)
-                    } else {
-                        responseCode = 200
-                        const iv = crypto.randomBytes(16).toString('hex').slice(0, 16);
-                        let key = crypto.createHash('sha256').update(String("informativeDonkey")).digest('base64').substr(0, 32);
-                        const cipher = crypto.createCipheriv("aes-256-ctr", key, iv)
-
-                        content = cipher.update(fs.readFileSync("." + urlObj.pathname), "utf-8", "hex")
-                        content += cipher.final("hex")
-                        contenttype = "text/hex"
-                    }
-                }
+            if (fs.existsSync("." + urlObj.pathname)) {
+                contenttype = "text/javascript"
+                content = fs.readFileSync("." + urlObj.pathname)
             }
         }
     }
